4a82d38fd33705eefd3697a432d2d1083d88173d
01zulfi <85733202+01zulfi@users.noreply.github.com>
2022-08-11 14:07:04 +0500

    Fix typo

--- server/vord/vord-route.js
+++ server/vord/vord-route.js
@@ -8,6 +8,6 @@ router.post("/", controller.addVord);
 router.get("/:id", controller.getVord);
 router.get("/user/:userId", controller.getUserVords);
 router.put("/:id", controller.updateVord);
-router.delete("/:id", controller.updateVord);
+router.delete("/:id", controller.deleteVord);
 
 module.exports = router;
