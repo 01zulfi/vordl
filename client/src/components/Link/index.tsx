26b826ff015ca7d532e8ceb728b4ed2a977ef609
01zulfi <85733202+01zulfi@users.noreply.github.com>
2022-08-17 19:40:36 +0500

    Create Link component

---
+++ client/src/components/Link/index.tsx
@@ -0,0 +1,37 @@
+import { FC } from 'react';
+import { NavLink, Link as RouterLink } from 'react-router-dom';
+
+interface LinkProps {
+  to: string;
+  children: React.ReactNode;
+  type: 'internal' | 'external';
+  styling: 'button' | 'link';
+  isNavLink?: boolean;
+}
+
+const Link: FC<LinkProps> = function Link({
+  to,
+  isNavLink,
+  type,
+  styling,
+  children,
+}) {
+  if (isNavLink) {
+    return <NavLink to={to}>{children}</NavLink>;
+  }
+
+  if (type === 'internal') {
+    return (
+      <RouterLink to={to} className={styling}>
+        {children}
+      </RouterLink>
+    );
+  }
+  return (
+    <a href={to} target="_blank" rel="noreferrer" className={styling}>
+      {children}
+    </a>
+  );
+};
+
+export default Link;
