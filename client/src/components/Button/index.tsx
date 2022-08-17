416ca67a2a6b232eee97121938144795bd4699a9
01zulfi <85733202+01zulfi@users.noreply.github.com>
2022-08-17 19:40:27 +0500

    Create Button component

---
+++ client/src/components/Button/index.tsx
@@ -0,0 +1,30 @@
+import { ReactNode, MouseEvent, FC } from 'react';
+
+interface ButtonProps {
+  children: React.ReactNode;
+  onClick: (e: MouseEvent<HTMLButtonElement>) => void;
+  variant: 'contained' | 'outlined';
+  color?: 'success' | 'error' | 'default';
+  disabled?: boolean;
+}
+
+const Button: FC<ButtonProps> = function Button({
+  children,
+  onClick,
+  variant,
+  disabled,
+  color,
+}) {
+  return (
+    <button
+      type="button"
+      className={`${variant} ${color}`}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
