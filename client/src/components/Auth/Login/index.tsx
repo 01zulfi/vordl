0accecd89dc61c6b87c3c0e46e60b851d59afe5a
01zulfi <85733202+01zulfi@users.noreply.github.com>
2022-08-23 18:18:58 +0500

    Update Login component * Add Form with Inputs & Button * Use useLogin hook * Add loading div & error handling

--- client/src/components/Auth/Login/index.tsx
+++ client/src/components/Auth/Login/index.tsx
@@ -1,7 +1,57 @@
-import { useState, useEffect } from 'react';
+import { FC, FormEvent, useState } from 'react';
+import Form from '../../Form';
+import Button from '../../Button';
+import Input, { onTextInputChange } from '../../Input';
+import useLogin from './use-login';
 
-function Login() {
-  return <h1>Login</h1>;
-}
+const Login: FC = function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { login, isLoading, isError, errors } = useLogin();
+
+  const onLoginFormSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    login({ email, password });
+  };
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <section>
+      <Form onSubmit={onLoginFormSubmit}>
+        <label htmlFor="email">
+          Email
+          <Input
+            id="email"
+            placeholder="Email"
+            type="email"
+            name="email"
+            value={email}
+            onChange={onTextInputChange(setEmail)}
+          />
+        </label>
+
+        <label htmlFor="password">
+          Password
+          <Input
+            id="password"
+            type="password"
+            name="password"
+            value={password}
+            onChange={onTextInputChange(setPassword)}
+          />
+        </label>
+
+        <Button variant="contained" type="submit">
+          Submit
+        </Button>
+
+        {isError && errors().map((error) => <div key={error}>{error}</div>)}
+      </Form>
+    </section>
+  );
+};
 
 export default Login;
